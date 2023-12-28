2591f3c273d4a18c634087832083b85704ba4c3e
dawpod <podrazad@gmail.com>
2023-12-28 18:09:33 +0100

    Modified TrainingApp and add Interfejsy i Klasy

--- TrainingApp/TrainingApp/Program.cs
+++ TrainingApp/TrainingApp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Cars;
 
 
 namespace TrainingApp
@@ -12,9 +13,31 @@ namespace TrainingApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Car car1 = new Car(2000,"audi");
+            Console.WriteLine(car1.jade());
+
 
             
+        }
+    }
+}
+namespace Cars
+{
+    public class Car
+    {
+        private int year;
+        private string mark;
+        
+        public Car(int year, string mark)
+        {
+            this.year = year;       
+            this.mark = mark;
+
+        }
+
+        public string jade()
+        {
+            return "Jade sobie";
         }
     }
 }
